e286ac2b2fa8166fe89b8d21c4442d6426f4f963
CORP\jason <jason@archidera.com>
2017-12-05 21:45:33 -0500

    Update aws commands for Windows

--- archidera.sh
+++ archidera.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://archidera.com --recursive --exclude=".git/*" --exclude=".gitignore" --exclude="*.sh"
+aws s3 cp D:\Users\jason\Documents\Workspace\companysitev2 s3://archidera.com --recursive --exclude=".git/*" --exclude=".gitignore" --exclude="*.sh"
--- xbrewer.sh
+++ xbrewer.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://xbrewer.com --recursive --exclude=".git/*" --exclude=".gitignore" --exclude="*.sh"
+aws s3 cp D:\Users\jason\Documents\Workspace\companysitev2 s3://xbrewer.com --recursive --exclude=".git/*" --exclude=".gitignore" --exclude="*.sh"
